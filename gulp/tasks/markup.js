77d6cd03340bdab2b38f31b2a5106c62f9469baf
lcb931023 <lcb931023@gmail.com>
2015-02-25 15:19:46 -0500

    jade for multi-page website

--- gulp/tasks/markup.js
+++ gulp/tasks/markup.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
+var fileinclude = require('gulp-file-include');
 var config = require('../config').markup
 var browserSync  = require('browser-sync');
 
 gulp.task('markup', function() {
   return gulp.src(config.src)
+    .pipe(fileinclude())
     .pipe(gulp.dest(config.dest))
     .pipe(browserSync.reload({stream:true}));
 });
